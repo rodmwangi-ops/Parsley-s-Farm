4af3dd11b6576e59f3f390ef16f3d8d44d30e6fd
rodmwangi-ops <rodmwangi@gmail.com>
2026-02-28 11:25:54 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,12 +6,12 @@
 const CONFIG = {
   // Google OAuth Client ID (from Google Cloud Console)
   // Step: SETUP.md → Section 2
-  GOOGLE_CLIENT_ID: 'YOUR_CLIENT_ID_HERE.apps.googleusercontent.com',
+  GOOGLE_CLIENT_ID: '376556459731-0njjva4bl5uol1vl0s1m9mcogjs1ebs0.apps.googleusercontent.com',
 
   // Google Sheet ID (from the spreadsheet URL)
   // The part between /d/ and /edit in the URL
   // e.g. https://docs.google.com/spreadsheets/d/THIS_PART_HERE/edit
-  SHEET_ID: 'YOUR_SHEET_ID_HERE',
+  SHEET_ID: '1cr7EAKGgMm3MWepOWisiqoYmMYi5UB0oHH2Dax3q7NQ',
 
   // Sheets API base URL
   SHEETS_API: 'https://sheets.googleapis.com/v4/spreadsheets',
