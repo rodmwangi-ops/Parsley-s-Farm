3441f7fc47ac647d47a6b813aeb16c32df5b4f4c
rodmwangi-ops <rodmwangi@gmail.com>
2026-02-28 12:05:57 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -30,6 +30,8 @@ const CONFIG = {
   // Allowed users (Google emails)
   ALLOWED_USERS: [
     'rodmwangi@gmail.com'
+    'lonahwanjama13@gmail.com'
+    'levywanke@gmail.com'
     // Add Joel and Joshua's emails when they have Google accounts
   ],
 
